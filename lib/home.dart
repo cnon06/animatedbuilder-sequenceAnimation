656ae307eb0aa285bf0fc9d8abe3ef788929e024
Sinan Alagöz <sinanalagoz74@gmail.com>
2023-01-27 09:22:39 +0300

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sequence_animation/flutter_sequence_animation.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late SequenceAnimation sequenceAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(vsync: this);
+    sequenceAnimation = SequenceAnimationBuilder()
+        .addAnimatable(
+            animatable: Tween<double>(begin: 0, end: 1),
+            from: Duration.zero,
+            to: Duration(seconds: 1),
+            tag: 'opacity1')
+        .addAnimatable(
+            animatable: Tween<double>(begin: 50, end: 150),
+            from: Duration(seconds: 1),
+            to: Duration(seconds: 2),
+            tag: 'width1')
+
+            .addAnimatable(
+            animatable: Tween<double>(begin: 50, end: 150),
+            from: Duration(seconds: 2),
+            to: Duration(seconds: 3),
+            tag: 'height1')
+
+
+             .addAnimatable(
+            animatable: BorderRadiusTween(begin: BorderRadius.circular(0), end: BorderRadius.circular(100)), 
+            from: Duration(seconds: 3),
+            to: Duration(seconds: 4),
+            curve: Curves.bounceInOut,
+            tag: 'radius1')
+
+
+            .addAnimatable(
+            animatable: ColorTween(begin: Colors.amber, end: Colors.blue), 
+            from: Duration(seconds: 4),
+            to: Duration(seconds: 5),
+            curve: Curves.easeIn,
+            tag: 'color1')
+        .animate(animationController);
+
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: animationController,
+              builder: (context, child) {
+                return Opacity(
+                  opacity: sequenceAnimation['opacity1'].value,
+                  child: Container(
+                     
+                    decoration: BoxDecoration(
+                      // border: Border.all(width: 1, color: Colors.pink),
+                      color: sequenceAnimation['color1'].value,
+                      borderRadius: sequenceAnimation['radius1'].value, //BorderRadius.circular(0),
+                    ),
+                   
+                    height: sequenceAnimation['height1'].value,
+                    width: sequenceAnimation['width1'].value,
+                    // child: Center(child: Text('Hello World')),
+                  ),
+                );
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: () {animationController.forward(from: 0);}, child: Text('Start'))
+          ],
+        ),
+      ),
+    );
+  }
+}
